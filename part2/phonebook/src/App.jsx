37d899e8738c8b264258d7c6b548ca63e998963a
MihaiBarascu <mihaialex_barascu@yahoo.com>
2024-08-13 14:38:53 +0300

    finalize exercises 2.01-2.15

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,51 +1,97 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
+import services from "./services/persons";
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+  const personsToShow =
+    filter === ""
+      ? persons
+      : persons.filter((person) =>
+          person.name.toLowerCase().includes(filter.toLowerCase())
+        );
 
-  const [filteredPersons, setFilteredPersons] = useState(persons);
+  useEffect(() => {
+    services.getAll().then((result) => {
+      setPersons(result);
+    });
+  }, []);
 
   const handleAdd = (event) => {
     event.preventDefault();
-    if (persons.some((person) => person.name === newName)) {
-      alert(`${newName} is allready added to phonebook`);
+    const person = persons.find((person) => person.name === newName);
+    if (person) {
+      updateNumber(person);
     } else if (persons.some((person) => person.number === newNumber)) {
       alert(`Number ${newNumber} is allready added to phonebook`);
     } else {
-      const newPersons = persons.concat({ name: newName, number: newNumber });
-      setPersons(newPersons);
+      const newPerson = { name: newName, number: newNumber };
+
+      services
+        .create(newPerson)
+        .then((person) => setPersons(persons.concat(person)));
+
       setNewNumber("");
       setNewName("");
-      setFilter("");
-      setFilteredPersons(newPersons);
     }
   };
+
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
+
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
   };
+
   const handleFilterChange = (event) => {
     const value = event.target.value;
     setFilter(value);
+  };
 
-    setFilteredPersons(
-      persons.filter((person) =>
-        person.name.toLowerCase().includes(value.toLowerCase())
-      )
+  const handleDelete = (id) => {
+    const person = persons.find((person) => person.id === id);
+    const confirmation = window.confirm(`Delete ${person.name} ?`);
+
+    if (confirmation) {
+      services
+        .remove(id)
+        .then(() => {
+          setPersons(persons.filter((person) => person.id != id));
+        })
+        .catch(() => {
+          alert(`${person.name} is allready deleted from phonebook`);
+        });
+    }
+  };
+
+  const updateNumber = (person) => {
+    const confirmation = window.confirm(
+      `${person.name} is allready added to phonebook, replace the old number with a new one?`
     );
+
+    if (confirmation) {
+      services
+        .update(person.id, { ...person, number: newNumber })
+        .then((updatedPerson) => {
+          setPersons(
+            persons.map((pers) =>
+              pers.id === updatedPerson.id ? updatedPerson : pers
+            )
+          );
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   };
 
   return (
@@ -61,7 +107,7 @@ const App = () => {
         onAdd={handleAdd}
       />
       <h3>Numbers</h3>
-      <Persons filteredPersons={filteredPersons} />
+      <Persons persons={personsToShow} onDelete={handleDelete} />
     </div>
   );
 };
---
+++ part2/phonebook/src/services/persons.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const url = "http://localhost:3001/persons";
+
+const getAll = () => {
+  return axios.get(url).then((response) => response.data);
+};
+
+const create = (newPerson) => {
+  return axios.post(url, newPerson).then((response) => response.data);
+};
+
+const remove = (id) => {
+  return axios.delete(`${url}/${id}`).then((response) => response.data);
+};
+
+const update = (id, newPerson) => {
+  return axios.put(`${url}/${id}`, newPerson).then((response) => response.data);
+};
+
+export default {
+  create,
+  remove,
+  update,
+  getAll,
+};
