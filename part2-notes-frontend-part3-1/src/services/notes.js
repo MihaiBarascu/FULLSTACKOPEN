6a4da773af5976093879b7d67f6afad2a0a31498
Mihai Barascu <mihaialex_barascu@yahoo.com>
2024-09-05 14:08:21 +0300

    add notes frontend back to project

---
+++ part2-notes-frontend-part3-1/src/services/notes.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+const baseUrl = "https:/phonebook-backend-fso-p3.fly.dev/api/notes";
+
+const getAll = () => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const create = (newObject) => {
+  const request = axios.post(baseUrl, newObject);
+  return request.then((response) => response.data);
+};
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject); //aici cred ca generaza axios eroarea pe care o prindem in app
+  return request.then((response) => response.data);
+};
+
+export default {
+  getAll,
+  create,
+  update,
+};
